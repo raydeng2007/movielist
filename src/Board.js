d658ccd3b2e6a78e57f6019fc853ea1281873c36
rayray <r6deng@uwaterloo.ca>
2017-11-12 23:39:24 -0500

    added delete and save button

--- src/Board.js
+++ src/Board.js
@@ -3,7 +3,7 @@
  */
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Movie from "./Movie";
+import './Movie.css';
 
 export default class Board extends Component {
 
@@ -19,17 +19,81 @@ export default class Board extends Component {
         }
     }
 
+    removeMovie(i){
+        var arr = this.state.movieTitles;
+        arr.splice(i,1);
+        this.setState({movieTitles:arr});
+    }
+
+    updateMovie(newText,i){
+        var arr = this.state.movieTitles;
+        arr[i] = newText;
+        this.setState({movieTitles:arr});
+    }
+
+    eachMovie(text,i) {
+        return(<Movie key={i} index={i} editMovie={this.updateMovie.bind(this)} deleteMovie={this.removeMovie.bind(this)}>{text}</Movie>)
+
+    }
+
     render(){
         return(
             <div className="board">
                 {
-                    this.state.movieTitles.map(function (text,i) {
-                        return(<Movie key={i}>{text}</Movie>)
-
-                    })
+                    this.state.movieTitles.map(this.eachMovie.bind(this))
                 }
             </div>
             );
     }
 
+}
+
+class Movie extends Component{
+    constructor(){
+        super();
+        this.state = {
+            editing: false,
+        };
+
+
+    }
+
+    edit(){
+        this.setState({editing:true});
+    };
+
+    delete(){
+        this.props.deleteMovie(this.props.index);
+    };
+
+    save(){
+        var movieName = this.refs.movieName.value;
+        this.props.editMovie(movieName,this.props.index);
+        this.setState({editing:false});
+
+    }
+
+    renderNormal(){
+        return (<div className="commentContainer">
+            <div className="commentText">{this.props.children}</div>
+            <button onClick={()=>{this.edit()}} className="button-primary">EDIT </button>
+            <button onClick={()=>{this.delete()}} className="button-danger">DELETE </button>
+        </div>);
+    }
+
+    renderForm(){
+        return (<div className="commentContainer">
+            <textarea ref="movieName" defaultValue= {this.props.children}></textarea>
+            <button onClick={()=>{this.save()}} className="button-primary">SAVE </button>
+        </div>);
+    }
+
+    render(){
+        if(this.state.editing){
+            return this.renderForm();
+        }else {
+            return this.renderNormal();
+        }
+
+    }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Movie from "./Movie";
 import Board from "./Board";
 
 ReactDOM.render(<Board></Board>,
